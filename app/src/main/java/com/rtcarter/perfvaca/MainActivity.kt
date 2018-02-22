9d31862db48cc8d243bb2348794a2955b2a1efa0
Ryan Carter <ryancarter87@gmail.com>
2018-02-22 13:14:37 -0500

    Added ScheduledActivity

--- app/src/main/java/com/rtcarter/perfvaca/MainActivity.kt
+++ app/src/main/java/com/rtcarter/perfvaca/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.rtcarter.perfvaca
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.DatePicker
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
-
-    val vacationList = arrayListOf<Any>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val preferences = getSharedPreferences ("data", Context.MODE_PRIVATE)
+
 
         dateText.text = "${datePicker.month+1}/${datePicker.dayOfMonth}/${datePicker.year}"
 
@@ -21,13 +24,20 @@ class MainActivity : AppCompatActivity() {
 
         datePicker.init(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH),
                 DatePicker.OnDateChangedListener { datePicker, year, month, day ->
-                    dateText.text = "${month+1}/${day}/${year}"
+                    dateText.text = "${month+1}/${day}/${year} "
+                    successText.text = ""
                 })
 
         scheduleBtn.setOnClickListener {
-            val newDay = dateText.text.toString()
-            vacationList.add(newDay)
+            val editor = preferences.edit()
+            editor.putString("scheduled", dateText.text.toString())
+            editor.commit()
             successText.text = "NEW VACATION DAY SCHEDULED"
+        }
+
+        leftBtn.setOnClickListener {
+            val intent = Intent(this, ScheduledActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/rtcarter/perfvaca/ScheduledActivity.kt
@@ -0,0 +1,12 @@
+package com.rtcarter.perfvaca
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ScheduledActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scheduled)
+    }
+}
