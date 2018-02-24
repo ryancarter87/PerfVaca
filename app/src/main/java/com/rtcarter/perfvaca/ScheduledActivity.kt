574e1095aba93cabdbd8d267f6114b62642f9495
Ryan Carter <ryancarter87@gmail.com>
2018-02-24 14:04:55 -0500

    Fixed the remove button. Now it removes the checked radio button from the layout, and also removes that value from the list and file

--- app/src/main/java/com/rtcarter/perfvaca/ScheduledActivity.kt
+++ app/src/main/java/com/rtcarter/perfvaca/ScheduledActivity.kt
@@ -26,9 +26,16 @@ class ScheduledActivity : AppCompatActivity() {
         val radiogroup = findViewById<View>(R.id.days_radio_group) as ViewGroup
 
         // List of strings to store lines from text file
+        // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!LOOK FOR A BETTER WAY TO DO THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        // Also create a mutable list to copy schedList to so that it can be edited
         var schedList = listOf("")
+        var schedMut = mutableListOf("")
+        // Use this string to store elements from schedMut so they can be written to file after
+        // removing an element with the removeBtn
+        var schedString = ""
 
         // Read text file, add each value to schedList list
+        // Then copy schedList to the mutable list schedMut
         if(fileList().contains("days.txt")) {
             try {
                 val file = InputStreamReader(openFileInput("days.txt"))
@@ -42,19 +49,20 @@ class ScheduledActivity : AppCompatActivity() {
                 br.close()
                 file.close()
                 schedList = all.split("\n").map { it.trim() }
-
-                // Possibly a much better way to do this, but I convert schedList to a mutable
-                // list here so the user can remove values using removeBtn.
+                // Copy schedList to a mutable list and remove the last element because it is a blank
+                // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!LOOK FOR A BETTER WAY TO DO ALL OF THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                schedMut = schedList.toMutableList()
+                schedMut.removeAt((schedList.size-1))
             }
             catch (e: IOException) {
             }
-        }
+       }
 
         // Iterate through values in schedList to add radio buttons for each
-        for (i in 0..(schedList.size-1)) {
+        for (i in 0..(schedMut.size-1)) {
             val button = RadioButton(this)
             button.setId(i)
-            button.setText(schedList[i])
+            button.setText(schedMut[i])
             radiogroup.addView(button)
         }
 
@@ -68,22 +76,38 @@ class ScheduledActivity : AppCompatActivity() {
         }
 
 
-        // NOT WORKING! ******************************************************************************
-
-        // Maybe add the inputreader for the text file after .remove()? I think the value is
-        // being removed from the array but the view isn't being updated. Also might be an issue
-        // with the format of ${selectedRadioButton.text}\n, that might not be how the values are stored
-        // in the list.
         removeBtn.setOnClickListener {
             val selectedId = days_radio_group.getCheckedRadioButtonId()
             val selectedRadioButton = findViewById<RadioButton>(selectedId)
-            if (schedList.contains(selectedRadioButton.text)) {
-                schedList.toMutableList().remove("${selectedRadioButton.text}\n")
+            if (schedMut.contains(schedMut[(selectedRadioButton.id + 1)])) {
+                // Remove the element at the specified location and remove the radio button
+                schedMut.removeAt(selectedRadioButton.id)
+                days_radio_group.removeView(selectedRadioButton)
+
+                // Iterate through schedMut and copy the values to schedString so they can be written
+                // to file
+                for (i in schedMut) {
+                    schedString += i + "\n"
+                }
+
+                 try {
+                    val file = OutputStreamWriter(openFileOutput("days.txt", Activity.MODE_PRIVATE))
+
+                    file.write (schedString)
+                    file.close ()
+
+                    Toast.makeText(this, "Successfully removed date from the list.", Toast.LENGTH_LONG).show()
+                }
+                 catch (e : IOException) {
+                }
             } else {
                 Toast.makeText(this, "Error: Couldn't find date in list.", Toast.LENGTH_LONG).show()
             }
         }
 
+
+        // Clear the file on click
+        // !!!!!!!!!!!!!!!!!!!!NEED TO ADD A CONFIRMATION BUTTON IN CASE THE USER ACCIDENTALLY PRESSES THIS!!!!!!!!!!!!!!!!!!!!!!!!
         clearBtn.setOnClickListener {
             try {
                 val file = OutputStreamWriter(openFileOutput("days.txt", Activity.MODE_PRIVATE))
